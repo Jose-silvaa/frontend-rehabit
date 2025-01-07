e16f5fecdf6dd03d46c8725caaa31fa1a06c8d46
Jose-silvaa <jvitorfacanha@gmail.com>
2025-01-07 18:27:21 -0300

    first-commit

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+
+const HeroSection = () => {
+    return (
+        <section className="bg-landing h-screen">
+            <h1 className="text-FontColor text-8xl font-semibold">Rehabit</h1>
+        </section>
+    )
+
+}
+
+
+export default HeroSection;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,23 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '3.815rem',
+      '7xl': '4.768rem', 
+      '8xl': '7rem'
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors : {
+        'landing' : "#0a0e31",
+        'FontColor' : '#b9b2ff'
+      }
     },
   },
   plugins: [],
