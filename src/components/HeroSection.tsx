594c4fb4efd7bc3847fb4f5a444c9bbf7dd6bdc6
Jose-silvaa <jvitorfacanha@gmail.com>
2025-01-10 19:12:07 -0300

    Upp Unstable

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
             <h1 className="text-FontColor text-6xl font-semibold">Rehabit</h1>
             <nav className="flex flex-col">
                 <Link href="">
-                    <button className="bg-ButtonColor text-white px-4 py-2 rounded-xl w-64">I HAVE AN ACCOUNT</button>
+                    <button className="bg-ButtonColor text-white px-4 py-2 rounded-xl w-64">I Have AN ACCOUNT</button>
                 </Link>
                 <Link href="/signup">
                     <button className="bg-ButtonColor text-white px-4 py-2 rounded-xl w-64 mt-4">SIGN UP</button>
